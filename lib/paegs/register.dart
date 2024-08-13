d23b5e380bedd5b482421dc522a316099027bd7e
pitchazerothree <161620273+pitchazerothree@users.noreply.github.com>
2024-08-13 20:55:32 +0700

    register.dart

--- lib/paegs/register.dart
+++ lib/paegs/register.dart
@@ -1,18 +1,12 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/config/internal_config.dart';
-//import 'package:flutter_application_2/config/config.dart';
-//import 'package:flutter_application_2/config/internal_config.dart';
-//import 'package:flutter_application_2/models/request/customer_login_post_req.dart';
 import 'package:flutter_application_2/models/request/customer_post_req.dart';
 import 'package:flutter_application_2/models/response/customersLoginRes.dart';
 import 'package:flutter_application_2/paegs/login.dart';
-import 'package:flutter_application_2/paegs/showtrip.dart';
 import 'package:http/http.dart' as http;
 
 class RegisterPage extends StatefulWidget {
-
   const RegisterPage({super.key});
 
   @override
@@ -20,195 +14,170 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  String url = '';
-  TextEditingController phoneNoCtl = TextEditingController();
   TextEditingController nameNoCtl = TextEditingController();
+  TextEditingController phoneNoCtl = TextEditingController();
   TextEditingController emailNoCtl = TextEditingController();
-  TextEditingController passNoCtl = TextEditingController();
+  TextEditingController passwordNoCtl = TextEditingController();
+  TextEditingController confirmPasswordNoCtl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('ลงทะเบียนสมาชิกใหม่'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  //ชื่อ-นามสกุล
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 280, 0),
-                    child: Text(
-                      'ชื่อ-นามสกุล',
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                          controller: nameNoCtl,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 1))))),
-
-                  // หมายเลขโทรศัพท์
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 220, 0),
-                    child: Text(
-                      'หมายเลขโทรศัพท์',
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                          controller: phoneNoCtl,
-                          // onChanged: (value) {
-                          //  phoneNo = value;
-                          // },
-                          keyboardType: TextInputType.phone,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 1))))),
-
-                  // อีเมล์
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 310, 0),
-                    child: Text(
-                      'อีเมล์',
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                          controller: emailNoCtl,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 1))))),
-
-                  // รหัสผ่าน
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 290, 0),
-                    child: Text(
-                      'รหัสผ่าน',
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                          controller: passNoCtl,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 1))))),
-
-                  // ยืนยันรหัสผ่าน
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 240, 0),
-                    child: Text(
-                      'ยืนยันรหัสผ่าน',
-                    ),
-                  ),
-                  const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 1))))),
-                ],
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 20.0), // Adjust the value to move the button down
-                  child: FilledButton(
-                    onPressed: login,
-                    child: const Text('สมัครสมาชิก'),
-                  ),
+      appBar: AppBar(
+        title: const Text('ลงทะเบียนสมาชิกใหม่'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              child: Text('ชื่อ-นามสกุล'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                controller: nameNoCtl,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
-              ]),
-
-              Row(
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Text('หมายเลขโทรศัพท์'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                controller: phoneNoCtl,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Text('อีเมล์'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                controller: emailNoCtl,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Text('รหัสผ่าน'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                controller: passwordNoCtl,
+                obscureText: true, // Hide password
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Text('ยืนยันรหัสผ่าน'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                controller: confirmPasswordNoCtl,
+                obscureText: true, // Hide password
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  TextButton(
-                      onPressed: () {
-                        register();
-                      },
-                      child: const Text(
-                        'หากมีบัญชีอยู่แล้ว?',
-                        style: TextStyle(color: Colors.black),
-                      )),
-                  TextButton(
-                      onPressed: () {
-                        register();
-                      },
-                      child: const Text('ลงทะเบียนใหม่')),
-                  // FilledButton(
-                  //     onPressed: login, child: const Text('เข้าสู่ระบบ')),
+                  FilledButton(
+                    onPressed: register,
+                    child: const Text('สมัครสมาชิก'),
+                  ),
                 ],
               ),
-              // Text(text),
-              // Text(text1)
-            ],
-          ),
-        ));
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text('หากมีบัญชีอยู่แล้ว?'),
+                  TextButton(
+                    onPressed: login,
+                    child: const Text('เข้าสู่ระบบ'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   void register() {
-    // log("This is Register button");
-    // setState(() {
-    //   text1 = 'Hello World!!!';
-    // });
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const RegisterPage(),
-        ));
-  }
-
-  void login() {
-    // Call login api
-    CustomerPostReq req = CustomerPostReq(
+    if (passwordNoCtl.text == confirmPasswordNoCtl.text &&
+        nameNoCtl.text.isNotEmpty &&
+        phoneNoCtl.text.isNotEmpty &&
+        emailNoCtl.text.isNotEmpty &&
+        passwordNoCtl.text.isNotEmpty &&
+        confirmPasswordNoCtl.text.isNotEmpty) {
+      
+      CustomerPostReq req = CustomerPostReq(
         fullname: nameNoCtl.text,
         phone: phoneNoCtl.text,
         email: emailNoCtl.text,
-        image: '',
-        password: passNoCtl.text);
+        image: "",
+        password: passwordNoCtl.text,
+      );
 
-    http
-        .post(Uri.parse("$API_ENDPOINT/customers"),
-            headers: {"Content-Type": "application/json; charset=utf-8"},
-            // Send json string of object model
-            body: customerPostReqToJson(req))
-        .then(
-      (value) {
-        // Convert Json String to Object (Model)
-        CustomersLoginPostResponse customer =
-            customersLoginPostResponseFromJson(value.body);
+      http.post(
+        Uri.parse("$API_ENDPOINT/customers"),
+        headers: {"Content-Type": "application/json; charset=utf-8"},
+        body: customerPostReqToJson(req),
+      ).then((response) {
+        log(response.body);
 
-        log(customer.customer.email);
-        Navigator.push(
+        if (response.statusCode == 200) {
+          // Successful registration
+          Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) =>  LoginPage(),
-            ));
-        // Convert Json String to Map<String, String
-        // var jsonRes = jsonDecode(value.body);
-        // log(jsonRes['customer']['email']);
-      },
-    ).catchError((eee) {
-      log(eee.toString());
-    });
-    // setState(() {
-    //   num++;
-    //   text = 'Login time: $num';
-    // });
-    //   log(phoneNoCtl.text);
-    //   // log(phoneNo);
-    //   Navigator.push(
-    //       context,
-    //       MaterialPageRoute(
-    //         builder: (context) => const ShowtripPage(),
-    //       ));
+            ),
+          );
+        } else {
+          // Handle error
+          log('Registration failed: ${response.statusCode}');
+        }
+      }).catchError((error) {
+        log('Error: $error');
+      });
+    } else {
+      log('Password and Confirm Password do not match');
+    }
+  }
+
+  void login() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>  LoginPage(),
+      ),
+    );
   }
 }
